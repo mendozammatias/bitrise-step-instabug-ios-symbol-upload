94d2a45952ee01e562305b3499bd18eebe483eda
mendozammatias- <matimendoza@frba.utn.edu.ar>
2024-02-12 15:51:59 -0300

    fix(step): fixed step var reference and some minor strings

--- step.sh
+++ step.sh
@@ -5,19 +5,19 @@ set -o pipefail
 echo "Instabug mapping files uploader"
 
 if [ ! -f $BITRISE_MAPPING_PATH ]; then
-    echo "Mapping file not found. Did you run this step before the build step?"
-    exit 0
+    echo "Mapping files not found. Did you run this step before the build step?"
+    exit 1
 fi
 
-echo "Mapping file found! Uploading..."
+echo "Mapping files found! Uploading..."
 
 ENDPOINT="https://api.instabug.com/api/sdk/v3/symbols_files"
 
-STATUS=$(curl "${ENDPOINT}" --write-out %{http_code} --silent --output /dev/null -F os=iOS -F symbols_file=@"${$BITRISE_DSYM_PATH}" -F application_token="${instabug_app_token}")
+STATUS=$(curl "${ENDPOINT}" --write-out %{http_code} --silent --output /dev/null -F os=iOS -F symbols_file=@"${BITRISE_DSYM_PATH}" -F application_token="${instabug_app_token}")
 
 if [ $STATUS -ne 200 ]; then
   echo "Error while uploading mapping files"
-  exit 0
+  exit 1
 fi
 
 echo "Success! Your mapping files got uploaded successfully"
